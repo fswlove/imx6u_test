3943c8937017d92dec48d4ea40ecf0a5ddd880e3
fswUbuntu20private <feng@ubuntu20.com>
2023-09-01 00:18:04 +0800

    补充项目文件

---
+++ demo/led/led.c
@@ -0,0 +1,7 @@
+//#include <stdio.h>
+//#include "led.h"
+
+void task_ledtest(void)
+{
+ //   printf("led test demo!");
+}
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "led.h"
+int main(void)
+{
+    task_ledtest();
+    return 0;
+}
